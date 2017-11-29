668988c8f414a9e6e5331f892c0043dcd874e935
Kathleen Nicholson <knichol8@nd.edu>
2017-11-29 11:19:50 -0500

    Added code for opening files

--- Translate_RNA_Script.py
+++ Translate_RNA_Script.py
@@ -3,7 +3,7 @@ import pandas
 import os
 import numpy 
 import csv 
-#open file codonmap and store it as a dictgionary under the variable name "d"
+#open file codonmap and store it as a dictionary under the variable name "d"
 d = {}
 with open('codonmap.txt', 'rb') as csv_file:
     for line in csv_file:
@@ -11,6 +11,9 @@ with open('codonmap.txt', 'rb') as csv_file:
         d[val] = key
 
 print(d)
-#read unique transcripts fasta file 
+#read transcript fasta files
+control1=open('control1.fasta', 'r')
+control2=open('control2.fasta', 'r')
+obese1=open('obese1.fasta', 'r')
+obese2=open('obese2.fasta', 'r')
 
-    
