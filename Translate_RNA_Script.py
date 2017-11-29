31d296feb7cb3828d203ec5c6f2b0d1d5b688e2c
Kathleen Nicholson <knichol8@nd.edu>
2017-11-29 11:04:15 -0500

    Edited pseudocode on Translate_RNA_Script.py

--- Translate_RNA_Script.py
+++ Translate_RNA_Script.py
@@ -3,8 +3,7 @@ import pandas
 import os
 import numpy 
 import csv 
-#open file codonmap, turn into dictionary?
-#read unique transcripts fasta file 
+#open file codonmap and store it as a dictgionary under the variable name "d"
 d = {}
 with open('codonmap.txt', 'rb') as csv_file:
     for line in csv_file:
@@ -12,4 +11,6 @@ with open('codonmap.txt', 'rb') as csv_file:
         d[val] = key
 
 print(d)
+#read unique transcripts fasta file 
+
     
