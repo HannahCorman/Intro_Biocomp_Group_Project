644097442f4d7c1a25b0b0617f79121e96fbf0f6
Katelyn Carothers <kcaroth1@nd.edu>
2017-12-01 15:28:52 -0500

    Fixed regex to include .fasta as the end of the line, otherwise it will find files that end with .fasta.align too

--- muscle_hmm_script.sh
+++ muscle_hmm_script.sh
@@ -1,7 +1,8 @@
 #Muscle alignment for protein files from BLAST
 #Path for muscle/hmmbuild may be different depending on where binaries are
 
-for sequence in $(ls | egrep '_[0-9]{1,2}\.fasta')
-	do ./muscle -in $sequence -out $sequence.align
+for sequence in $(ls | egrep '_[0-9]{1,2}\.fasta$')
+	do 
+	./muscle -in $sequence -out $sequence.align
 	done
 
