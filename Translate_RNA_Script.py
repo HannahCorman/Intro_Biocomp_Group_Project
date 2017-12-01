891db62ff24f131cb5c6cc3cef8595487340d277
Grant Keller <gkeller1@nd.edu>
2017-12-01 11:26:00 +0100

    Added translate function, which translates a DNA fasta file into a ready-to-write protein fasta file. Not sure if output is in the right format. Also included a single call to translate under main function which outputs a protein fasta for control1.

--- Translate_RNA_Script.py
+++ Translate_RNA_Script.py
@@ -1,19 +1,45 @@
 #import packages
+from __future__ import print_function
 import pandas
 import os
-import numpy 
-import csv 
+import numpy
+import csv
+
 #open file codonmap and store it as a dictionary under the variable name "d"
 d = {}
-with open('codonmap.txt', 'rb') as csv_file:
+with open('codonmap.txt', 'r') as csv_file: # Soudn't open as binary, ASCII is fine.
     for line in csv_file:
-        (aminoacid, codon) = line.split()
-        d[codon] = aminoacid
+        aa, codon = line.split() # Don't need parentheses for simultaneous assignment
+        d[codon] = aa
 
-print(d)
-#read transcript fasta files
-control1=open('control1.fasta', 'r')
-control2=open('control2.fasta', 'r')
-obese1=open('obese1.fasta', 'r')
-obese2=open('obese2.fasta', 'r')
+def translate(d, fasta):
+    """
+    When passed a full fasta file split by line (i.e. file.read().split()),
+    this function translates DNA to Protein up to an 
+    Amber (TAG), Ochre (TAA), or Umber (TGA) stop codon.
+    Returns a list of tab delimited NAME-SEQUENCE pairs.
+    """
+    sequences = [] # sequential list of protein sequences
+    sequence_names = []
+    for i, item in enumerate(fasta): # loops over list, list items
+        protein = '' # translated protein sequence
+        if i%2 == 0: # if index is even
+            sequence_names.append(item)
+        else:
+            for j in range(0,len(y[1]),3):
+                aa = d[item[j:j+3]]
+                if aa == 'Stop':
+                    break
+                else:
+                    protein += aa
+            sequences.append(protein)
+    return ['{0}\t{1}'.format(sequence_names[i], sequences[i]) for i in range(len(sequences))]
 
+if __name__ == '__main__':
+    #read transcript fasta files
+    control1=open('control1.fasta', 'r')
+    control2=open('control2.fasta', 'r')
+    obese1=open('obese1.fasta', 'r')
+    obese2=open('obese2.fasta', 'r')
+    
+    print('\n'.join(translate(d,control1.read().split())))
