bbd14a0379968bd2808da1a9fdaf9eeba533828c
Grant Keller <gkeller1@nd.edu>
2017-12-01 11:37:56 +0100

    Improved namespace modularity.

--- Translate_RNA_Script.py
+++ Translate_RNA_Script.py
@@ -1,9 +1,10 @@
 #import packages
 from __future__ import print_function
-import pandas
-import os
-import numpy
 import csv
+import os
+import pandas
+import numpy
+
 
 #open file codonmap and store it as a dictionary under the variable name "d"
 d = {}
@@ -12,10 +13,10 @@ with open('codonmap.txt', 'r') as csv_file: # Soudn't open as binary, ASCII is f
         aa, codon = line.split() # Don't need parentheses for simultaneous assignment
         d[codon] = aa
 
-def translate(d, fasta):
+def translate(codex, fasta):
     """
     When passed a full fasta file split by line (i.e. file.read().split()),
-    this function translates DNA to Protein up to an 
+    this function translates DNA to Protein up to an
     Amber (TAG), Ochre (TAA), or Umber (TGA) stop codon.
     Returns a list of tab delimited NAME-SEQUENCE pairs.
     """
@@ -26,20 +27,20 @@ def translate(d, fasta):
         if i%2 == 0: # if index is even
             sequence_names.append(item)
         else:
-            for j in range(0,len(y[1]),3):
-                aa = d[item[j:j+3]]
-                if aa == 'Stop':
+            for j in range(0, len(item), 3):
+                res = codex[item[j:j+3]]
+                if res == 'Stop':
                     break
                 else:
-                    protein += aa
+                    protein += res
             sequences.append(protein)
-    return ['{0}\t{1}'.format(sequence_names[i], sequences[i]) for i in range(len(sequences))]
+    return ['{0}\t{1}'.format(sequence_names[p], sequences[p]) for p in range(len(sequences))]
 
 if __name__ == '__main__':
     #read transcript fasta files
-    control1=open('control1.fasta', 'r')
-    control2=open('control2.fasta', 'r')
-    obese1=open('obese1.fasta', 'r')
-    obese2=open('obese2.fasta', 'r')
-    
-    print('\n'.join(translate(d,control1.read().split())))
+    control1 = open('control1.fasta', 'r')
+    control2 = open('control2.fasta', 'r')
+    obese1 = open('obese1.fasta', 'r')
+    obese2 = open('obese2.fasta', 'r')
+
+    print('\n'.join(translate(d, control1.read().split())))
