4f3d092d21703374ab98cab7385b3eed80958aee
Katelyn Carothers <kcaroth1@nd.edu>
2017-12-01 15:38:13 -0500

    Added for loop to build hmm from alignment files

--- muscle_hmm_script.sh
+++ muscle_hmm_script.sh
@@ -1,8 +1,13 @@
 #Muscle alignment for protein files from BLAST
+#Build hmm for alignments
 #Path for muscle/hmmbuild may be different depending on where binaries are
 
-for sequence in $(ls | egrep '_[0-9]{1,2}\.fasta$')
+for sequence in $(ls | egrep '_[0-9]{1,2}\.fasta$') #grep only fasta files from BLAST search, not translated RNAseq files
 	do 
 	./muscle -in $sequence -out $sequence.align
 	done
 
+for alignment in $(ls | egrep '_[0-9]{1,2}\.fasta.align$') #grep only alignment files
+	do
+	./hmmbuild $alignment.hmm $alignment
+	done
