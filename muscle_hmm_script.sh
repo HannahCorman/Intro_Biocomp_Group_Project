596fb9f1d959a4d44af1636fbc8c940d9b5b0fe7
Katelyn Carothers <kcaroth1@nd.edu>
2017-12-01 11:09:56 -0500

    Updated bash script for muscle alignment

--- muscle_hmm_script.sh
+++ muscle_hmm_script.sh
@@ -1,1 +1,5 @@
-#Muscle alignment for protein files
+#Muscle alignment for protein files from BLAST
+#Path for muscle/hmmbuild may be different depending on where binaries are
+
+for sequence in *.fasta
+./muscle -in $sequence.fasta -out $sequence.align
