b0d94026fb471af0907d71d7fe8900a4792f7c65
Katelyn Carothers <kcaroth1@nd.edu>
2017-12-01 15:01:15 -0500

    Bash script now works for muscle alignment of six protein file from BLAST

--- muscle_hmm_script.sh
+++ muscle_hmm_script.sh
@@ -1,5 +1,7 @@
 #Muscle alignment for protein files from BLAST
 #Path for muscle/hmmbuild may be different depending on where binaries are
 
-for sequence in *.fasta
-./muscle -in $sequence.fasta -out $sequence.align
+for sequence in $(ls | egrep '_[0-9]{1,2}\.fasta')
+	do ./muscle -in $sequence -out $sequence.align
+	done
+
