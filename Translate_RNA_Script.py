c1901396b3de8979e364ea2c538da84de7c8bc2c
Grant Keller <gkeller1@nd.edu>
2017-12-03 17:50:07 -0500

    Reformatted translate() output, cleaned up main function by iterating over filenames and handling open() close() calls with a with statement.

--- Translate_RNA_Script.py
+++ Translate_RNA_Script.py
@@ -22,7 +22,7 @@ def translate(codex, fasta):
     """
     sequences = [] # sequential list of protein sequences
     sequence_names = []
-    for i, item in enumerate(fasta): # loops over list, list items
+    for i, item in enumerate(fasta.read().split()): # loops over list, list items
         protein = '' # translated protein sequence
         if i%2 == 0: # if index is even
             sequence_names.append(item)
@@ -34,32 +34,12 @@ def translate(codex, fasta):
                 else:
                     protein += res
             sequences.append(protein)
-    return ['{0}\n{1}'.format(sequence_names[p], sequences[p]) for p in range(len(sequences))]
+    return '\n'.join(['{0}\n{1}'.format(sequence_names[p], sequences[p]) for p in range(len(sequences))])
 
 if __name__ == '__main__':
     #read transcript fasta files
-    control1 = open('control1.fasta', 'r')
-    control2 = open('control2.fasta', 'r')
-    obese1 = open('obese1.fasta', 'r')
-    obese2 = open('obese2.fasta', 'r')
-    #creates and opens files to write
-    control1protein = open('control1protein.fasta', 'w')
-    control2protein = open('control2protein.fasta', 'w')
-    obese1protein = open('obese1protein.fasta', 'w')
-    obese2protein = open('obese2protein.fasta', 'w')
-    #writes the translated nucleotides to the outfiles
-    control1protein.write('\n'.join(translate(d, control1.read().split())))
-    control2protein.write('\n'.join(translate(d, control2.read().split())))
-    obese1protein.write('\n'.join(translate(d, obese1.read().split())))
-    obese2protein.write('\n'.join(translate(d, obese2.read().split())))
-    #closes files
-    control1.close
-    control2.close
-    obese1.close
-    obese2.close
-    control1protein.close
-    control2protein.close
-    obese1protein.close
-    obese2protein.close
-    
-    
+    condition_list = ['control1', 'control2', 'obese1', 'obese2']
+    for condition in condition_list:
+        with open('%s.fasta'%condition, 'r') as inFile, \
+        open('%sprotein.fasta'%condition, 'w') as outFile:
+            outFile.write(translate(d, inFile))
